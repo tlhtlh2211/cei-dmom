cf7257d66ad60eb730c26ff43b3d58104a8404cf
tlhtlh2211 <89590444+tlhtlh2211@users.noreply.github.com>
2025-07-16 14:01:01 +0700

    update figure

--- vietnam-plot/plot.py
+++ vietnam-plot/plot.py
@@ -31,29 +31,29 @@ not_ready_gdf = vn_data[vn_data['Ready'] == False]
 ready_source = GeoJSONDataSource(geojson=ready_gdf.to_json())
 not_ready_source = GeoJSONDataSource(geojson=not_ready_gdf.to_json())
 
-# --- Blue Color Mapper for 'Ready' Provinces ---
+# --- Green Color Mapper for 'Ready' Provinces ---
 # We slice and reverse the palette to ensure higher values get darker colors.
-palette_blue = brewer['Blues'][7][:4][::-1]
-blue_color_mapper = LinearColorMapper(palette=palette_blue, low=80, high=100)
+palette_green = brewer['Greens'][7][:4][::-1]
+green_color_mapper = LinearColorMapper(palette=palette_green, low=80, high=100)
 
-ticks_blue = list(range(80, 101, 4))
-tick_labels_blue = {str(tick): str(tick) for tick in ticks_blue}
+ticks_green = list(range(80, 101, 4))
+tick_labels_green = {str(tick): str(tick) for tick in ticks_green}
 
-blue_color_bar = ColorBar(
-    color_mapper=blue_color_mapper,
+green_color_bar = ColorBar(
+    color_mapper=green_color_mapper,
     label_standoff=8,
     width=850, 
     height=20,
     border_line_color=None, 
     location=(0,0), 
     orientation='horizontal', 
-    major_label_overrides=tick_labels_blue,
+    major_label_overrides=tick_labels_green,
     title="Ready"
 )
 
 # --- Red Color Mapper for 'Not Ready' Provinces ---
-# Slicing and reversing the palette for consistency with the blue scale.
-palette_red = brewer['Reds'][7][:4][2:]
+# Slicing and reversing the palette for consistency with the green scale.
+palette_red = brewer['YlOrRd'][7][:4]
 red_color_mapper = LinearColorMapper(palette=palette_red, low=60, high=80)
 
 ticks_red = list(range(60, 81, 4))
@@ -107,18 +107,18 @@ p.patches(
     fill_alpha=1
 )
 
-# Plot 'ready' provinces in blue with intensity
+# Plot 'ready' provinces in green with intensity
 p.patches(
     xs='xs',
     ys='ys', 
     source=ready_source,
-    fill_color={'field' :'plot_data', 'transform' : blue_color_mapper},
+    fill_color={'field' :'plot_data', 'transform' : green_color_mapper},
     line_color='black', 
     line_width=0.25, 
     fill_alpha=1
 )
 
-p.add_layout(blue_color_bar, 'below')
+p.add_layout(green_color_bar, 'below')
 p.add_layout(red_color_bar, 'above')
 
 from bokeh.io import export_png
